a84129e1fda59459467baa81d5347b85ccf3132c
Nagasai-1755 <147495347+Nagasai-1755@users.noreply.github.com>
2023-10-15 11:56:39 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,16 @@
+package com.first;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int n=25;
+		int fact=1;
+		for(int i=1;i<=n;i++) {
+			fact=fact*i;
+		}
+		System.out.println("factorial ="+fact);
+		// TODO Auto-generated method stub
+
+	}
+
+}
